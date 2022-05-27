80a43b18a4a71a7d5ac0b62e3d64da9336f0cf20
Reshkin <maksimtereschkn@gmail.com>
2022-05-28 00:18:36 +0300

    Initial commit

---
+++ camera/src/main/java/ru/mirea/tereshkin/camera/MainActivity.java
@@ -0,0 +1,18 @@
+package ru.mirea.tereshkin.camera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onImageViewClick(View view) {
+    }
+}
